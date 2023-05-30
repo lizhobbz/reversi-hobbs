1f88856090fbf920ed5484ca9293df76c41b2aaa
Liz Hobbs <eghobbs@uci.edu>
2023-05-30 15:49:18 -0500

    no message

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -43,6 +43,19 @@ socket.on('join_room_response', (payload) =>{
         console.log(payload.message);
         return;
     }
+
+    /* If we are being notified of ourselves, then ignore the message and return */
+    if (payload.socket_id === socket.id){
+        return;
+    }
+
+    let domElements = $('.socket_'+payload.socket_id);
+    /* If we are being repeat notified then return */
+
+    if (domElements.length !== 0){
+        return;
+    }
+
     /* 
 
         <div class="row align-items-center">
@@ -94,7 +107,15 @@ socket.on('player_disconnected', (payload) =>{
         console.log('Server did not send a payload');
         return;
     }
-    let newHTML = '<p class=\'left_room_response\'>'+payload.username+' left the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
+    if(payload.socket_id === socket.id){
+        return;
+    }
+    let domElements = $('.socket_'+payload.socket_id);
+    if(domElements.length !== 0){
+        domElements.hide("fade",500);
+    }
+
+    let newHTML = '<p class=\'left_room_response\'>' + payload.username +' left the '+payload.room+'. (There are '+ payload.count +' users in this room)</p>';
     let newNode = $(newHTML);
     newNode.hide();
     $('#messages').prepend(newNode);
